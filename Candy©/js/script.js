5e53839f26a9b306faf4efe1c7d24c992f2a3c0e
Candygame <britopt3@gmail.com>
2016-11-16 20:14:19 +0000

    Add files via upload

---
+++ Candy©/js/script.js
@@ -0,0 +1,34 @@
+$(document).ready(function(){
+
+	$("#banner").css({"height":$(window).height() + "px"});
+
+	var flag = false;
+	var scroll;
+
+	$(window).scroll(function(){
+		scroll = $(window).scrollTop();
+
+		if(scroll > 200){
+			if(!flag){
+				$("#logo").css({"margin-top": "-5px", "width": "80px","height":"60px", "transition": "1.5s"});
+
+				$("header").css({"background-color": "#3c3c3c"});
+				flag = true;
+			}
+		}else{
+			if(flag){
+				$("#logo").css({"margin-top": "150px", "width": "250px","height":"250px"});
+
+				$("header").css({"background-color": "transparent"});
+				flag = false;
+			}
+		}
+
+
+	});
+
+});
+
+$(document).ready(function(){
+console.log("index.php esta preparado para iniciar.");
+});
